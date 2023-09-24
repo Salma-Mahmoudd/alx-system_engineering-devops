95d0d7dbc3577b4564672adf29c871120d2004bf
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-09-24 16:58:38 -0700

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Puppet to make changes to our configuration file.
 exec { 'ConfigPuppet':
-  command => 'echo "\tPasswordAuthentication no\n\tIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
+  command => 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
   path    => '/usr/bin/'
 }
